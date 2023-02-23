aa79dfc578981ad3383dba91bf504bcdbba02431
azim128 <azimruet28@gmail.com>
2023-02-23 15:55:48 +0600

    deploy

--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import './header.css'
-import People from "../../assets/people.png"
-import Ai from "../../assets/ai.png"
-
-  
+import React from "react";
+import Ai from "../../assets/ai.png";
+import People from "../../assets/people.png";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding'>
-      <div className='gpt3__header-content'>
-        <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
-        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-        <div className='gpt3__header-content__input'>
-          <input type="email"placeholder='Your E-mail Address' />
-          <button type='button'>Get Start</button>
+    <div className="gpt3__header section__padding">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed an for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your E-mail Address" />
+          <button type="button">Get Start</button>
         </div>
-        <div className='gpt3__header-content__people'>
+        <div className="gpt3__header-content__people">
           <img src={People} alt="People" />
           <p>1,600 People requested access a visit in last 24 hours</p>
-        </div> 
+        </div>
       </div>
 
-      <div className='gpt3__header-image'>
-        <img src={Ai} alt="Ai"/>
+      <div className="gpt3__header-image">
+        <img src={Ai} alt="Ai" />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
